5f3c14e212e8171162bb473143abd294b46b49e1
Stephen McLaughry <stephen.mclaughry@jivesoftware.com>
2012-09-26 14:48:00 -0700

    Making the magic work on linux (at least on ubuntu, or any distro with espeak installed)

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ var spawn 	= require('child_process').spawn;
 app.use(express.bodyParser());
 
 app.post('/', function(req, res){
+	console.log('you said ' + req.body.say + ' to ' + process.platform);
 
-	console.log('you said ' + req.body.say);
-
-	var say = spawn('say', ['\''+req.body.say+'\'']);
+	var say;
+	if (process.platform = 'linux') {
+		say = spawn('espeak', ['\''+req.body.say+'\'']);
+	} else {
+		say = spawn('say', ['\''+req.body.say+'\'']);
+	}
 
 	say.on('exit', function(){
 		res.end('success');	
